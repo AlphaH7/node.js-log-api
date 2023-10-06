946456600baf6c0be4cae51ff95469a65e3a2815
AlphaH7 <Alistier.hea@gmail.com>
2023-10-06 04:19:42 +0100

    DEV: enabled password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/user';
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (req, res) => {
     const { name, email, password, confirmPassword } = req.body;
@@ -18,9 +19,37 @@ export const registerUser = async (req, res) => {
         return res.status(400).send('Password and Confirm Password do not match.');
     }
 
-    const user = new User({ name, email, password });
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({ name, email, password: hashedPassword });
     
     await user.save();
 
     res.status(201).send({ message: 'User registered successfully!' });
+};
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if the email exists in the database
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send('Invalid email or password.');
+        }
+
+        // Compare the provided password with the hashed password in the database
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(400).send('Invalid email or password.');
+        }
+
+        // If all is good, handle login (e.g., create a session, return a JWT, etc.)
+        // For demonstration, we'll just send a success message.
+        res.send({ message: 'Logged in successfully!' });
+    } catch (error) {
+        res.status(500).send({ message: 'Server Error.' });
+    }
 };
